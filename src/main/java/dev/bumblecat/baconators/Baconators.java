f02859b7bc404f63c1b628ae48a7c8d03ee121ea
Baijson <baijson@gmail.com>
2022-01-17 00:17:53 +0100

    Initial commit

---
+++ src/main/java/dev/bumblecat/baconators/Baconators.java
@@ -0,0 +1,8 @@
+package dev.bumblecat.baconators;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Baconators.MOD)
+public class Baconators {
+    public static final String MOD = "baconators";
+}
