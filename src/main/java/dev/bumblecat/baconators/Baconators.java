470a609f927b1edf5f124967e236e63b2b7bb237
Baijson <baijson@gmail.com>
2022-01-17 00:49:49 +0100

    Initial commit

--- src/main/java/dev/bumblecat/baconators/Baconators.java
+++ src/main/java/dev/bumblecat/baconators/Baconators.java
@@ -1,8 +1,26 @@
 package dev.bumblecat.baconators;
 
+import dev.bumblecat.baconators.common.objects.items.Baconator;
+import dev.bumblecat.bumblecore.common.objects.items.Variables;
+import dev.bumblecat.bumblecore.common.register.Registry;
+import dev.bumblecat.foodsicles.Foodsicles;
+
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ObjectHolder;
 
 @Mod(Baconators.MOD)
 public class Baconators {
     public static final String MOD = "baconators";
+
+    @ObjectHolder("baconator")
+    public static final Baconator
+            BACONATOR = null;
+
+    public Baconators() {
+        Registry.register("baconator", ()
+                -> new Baconator(new Variables()
+                .setStackSize(1)
+                .setCreativeTab(Foodsicles.creativeTab))
+        );
+    }
 }
---
+++ src/main/java/dev/bumblecat/baconators/common/objects/items/Baconator.java
@@ -0,0 +1,12 @@
+package dev.bumblecat.baconators.common.objects.items;
+
+import dev.bumblecat.bumblecore.common.objects.items.Variables;
+import dev.bumblecat.foodsicles.common.objects.items.Foodsicle;
+import dev.bumblecat.foodsicles.common.objects.items.IFoodsicle;
+
+public class Baconator extends Foodsicle implements IFoodsicle {
+
+    public Baconator(Variables variables) {
+        super(variables);
+    }
+}
