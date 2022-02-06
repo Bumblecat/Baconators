827ef874df02418e7e35e2828b0940f851f841d2
Baijson <baijson@gmail.com>
2022-02-06 10:48:15 +0100

    moved registration to class.

--- src/main/java/dev/bumblecat/baconators/Baconators.java
+++ src/main/java/dev/bumblecat/baconators/Baconators.java
@@ -1,5 +1,6 @@
 package dev.bumblecat.baconators;
 
+import dev.bumblecat.baconators.common.ObjectHolders;
 import dev.bumblecat.baconators.common.objects.items.Baconator;
 import dev.bumblecat.bumblecore.common.objects.items.Variables;
 import dev.bumblecat.bumblecore.common.register.Registry;
@@ -12,28 +13,7 @@ import net.minecraftforge.registries.ObjectHolder;
 public class Baconators {
     public static final String MOD = "baconators";
 
-    @ObjectHolder("baconator")
-    public static final Baconator
-            BACONATOR = null;
-
-    @ObjectHolder("darkosto")
-    public static final Baconator
-            DARKOSTO = null;
-
-    @ObjectHolder("matrixis")
-    public static final Baconator
-            MATRIXIS = null;
-
     public Baconators() {
-
-        Registry.register("baconator", ()
-                -> new Baconator(new Variables().setStackSize(1).setCreativeTab(Foodsicles.creativeTab))
-        );
-        Registry.register("darkosto", ()
-                -> new Baconator(new Variables().setStackSize(1).setCreativeTab(Foodsicles.creativeTab))
-        );
-        Registry.register("matrixis", ()
-                -> new Baconator(new Variables().setStackSize(1).setCreativeTab(Foodsicles.creativeTab))
-        );
+        ObjectHolders.register();
     }
 }
---
+++ src/main/java/dev/bumblecat/baconators/common/ObjectHolders.java
@@ -0,0 +1,34 @@
+package dev.bumblecat.baconators.common;
+
+import dev.bumblecat.baconators.Baconators;
+import dev.bumblecat.baconators.common.objects.items.Baconator;
+import dev.bumblecat.bumblecore.common.objects.items.Variables;
+import dev.bumblecat.bumblecore.common.register.Registry;
+import dev.bumblecat.foodsicles.Foodsicles;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+@ObjectHolder(Baconators.MOD)
+public class ObjectHolders {
+
+    @ObjectHolder("baconator")
+    public static final Baconator BACONATOR = null;
+
+    @ObjectHolder("darkosto")
+    public static final Baconator DARKOSTO = null;
+
+    @ObjectHolder("matrixis")
+    public static final Baconator MATRIXIS = null;
+
+    public static void register() {
+        Registry.register("baconator", ()
+                -> new Baconator(new Variables().setStackSize(1).setCreativeTab(Foodsicles.creativeTab))
+        );
+        Registry.register("darkosto", ()
+                -> new Baconator(new Variables().setStackSize(1).setCreativeTab(Foodsicles.creativeTab))
+        );
+        Registry.register("matrixis", ()
+                -> new Baconator(new Variables().setStackSize(1).setCreativeTab(Foodsicles.creativeTab))
+        );
+    }
+}
